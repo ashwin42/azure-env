449734eac1ccff7c7323df9f8f92606ac484d44b
Andreas Björshammar <andreas.bjorshammar@northvolt.com>
2019-01-17 15:07:52 +0100

    Added template for DNS management

---
+++ nv-core/dns_northvolt.com.tf
@@ -0,0 +1,21 @@
+# resource "azurerm_dns_zone" "nortvolt_com" {
+#   name                = "northvolt.com"
+#   resource_group_name = "${var.resource_group_name}"
+#   zone_type           = "Public"
+# }
+
+# resource "azurerm_dns_a_record" "northvolt_com" {
+#   name                = ""
+#   zone_name           = "${azurerm_dns_zone.nortvolt_com.name}"
+#   resource_group_name = "${var.resource_group_name}"
+#   ttl                 = 3600
+#   records             = ["35.204.11.13"]
+# }
+
+# resource "azurerm_dns_cname_record" "www_northvolt_com" {
+#   name                = "www"
+#   zone_name           = "${azurerm_dns_zone.nortvolt_com.name}"
+#   resource_group_name = "${var.resource_group_name}"
+#   ttl                 = 3600
+#   record              = "northvolt.com"
+# }
