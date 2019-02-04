34c487e11d10f401f0ee80fd3832e63c556b61bf
Andreas Björshammar <andreas.bjorshammar@northvolt.com>
2019-02-04 17:44:06 +0100

    Added nv-lab resource group

---
+++ nv-lab/network.tf
@@ -0,0 +1,4 @@
+locals {
+  nv_lab_1 = "${data.terraform_remote_state.nv-core.subnet_id.nv_shared_1}"
+}
+
---
+++ nv-lab/variables.tf
@@ -0,0 +1,7 @@
+variable "resource_group_name" {
+  default = "nv-lab"
+}
+
+variable "location" {
+  default = "northeurope"
+}
