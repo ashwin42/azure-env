9fcb78c8354b91bbf89b509cab249b70f379f1ce
Christoffer Jonsson <christoffer.jonsson@northvolt.com>
2019-10-03 10:56:15 +0200

    Added Azure/ABB VNET & Subnets

--- nv-production/800xa/network.tf
+++ nv-production/800xa/network.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "abb_800xa" {
   resource_group_name = azurerm_resource_group.abb_800xa.name
   location            = var.location
   name                = "800xa"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.60.0.0/16"]
   # Once AADDS is in place, this should be enabled/changed
   #dns_servers         = ["10.101.250.4", "10.101.250.5"]
   tags                = merge(var.default_tags, {})
@@ -14,5 +14,13 @@ resource "azurerm_subnet" "abb_800xa_1" {
   resource_group_name  = azurerm_resource_group.abb_800xa.name
   virtual_network_name = azurerm_virtual_network.abb_800xa.name
   name                 = "800xa-1"
-  address_prefix       = "10.0.1.0/24"
+  address_prefix       = "10.60.50.0/24"
+}
+
+# 800xa subnets
+resource "azurerm_subnet" "abb_800xa_2" {
+  resource_group_name  = azurerm_resource_group.abb_800xa.name
+  virtual_network_name = azurerm_virtual_network.abb_800xa.name
+  name                 = "800xa-1"
+  address_prefix       = "10.60.60.0/24"
 }
