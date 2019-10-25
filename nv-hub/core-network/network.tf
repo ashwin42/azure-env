73ede28f9102cc74954b0bf26d7840b78f214ba5
sidekicke <55448174+sidekicke@users.noreply.github.com>
2019-10-25 12:17:04 +0200

    Ergateway (#9)
    * added gateway subnet
    
    * added er gateway
    
    * added public ip

--- nv-hub/core-network/network.tf
+++ nv-hub/core-network/network.tf
@@ -17,3 +17,34 @@ resource "azurerm_virtual_network_peering" "nv-hub_to_800xa" {
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
 }
+
+resource "azurerm_subnet" "GatewaySubnet" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_resource_group.core_network.name
+  virtual_network_name = azurerm_virtual_network.core_vnet.name
+  address_prefix       = "10.40.254.0/24"
+}
+
+resource "azurerm_public_ip" "public-er-gw" {
+  name                = "public-er-gw"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.core_network.name
+
+  allocation_method = "Dynamic"
+}
+
+resource "azurerm_virtual_network_gateway" "nv-hub-er-gw" {
+  name                = "nw-hub-er-gw"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.core_network.name
+
+  type = "ExpressRoute"
+
+  sku = "Standard"
+
+  ip_configuration {
+    name                 = "er-gw-config"
+    public_ip_address_id = azurerm_public_ip.public-er-gw.id
+    subnet_id            = azurerm_subnet.GatewaySubnet.id
+  }
+}
