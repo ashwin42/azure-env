1ab2efb121475f67aa48a80c36cfa85ad097a11d
sidekicke <55448174+sidekicke@users.noreply.github.com>
2019-10-25 14:11:22 +0200

    added gateway connection & er authorization (#10)

--- nv-hub/core-network/express_route.tf
+++ nv-hub/core-network/express_route.tf
@@ -17,3 +17,20 @@ resource "azurerm_express_route_circuit" "main" {
 output "labs_express_route_skey" {
   value = azurerm_express_route_circuit.main.service_key
 }
+
+resource "azurerm_express_route_circuit_authorization" "er-to-er-gw" {
+  name                       = "er-to-er-gw"
+  express_route_circuit_name = azurerm_express_route_circuit.main.name
+  resource_group_name        = azurerm_resource_group.core_network.name
+}
+
+resource "azurerm_virtual_network_gateway_connection" "er-gw-to-er" {
+  name                = "er-gw-to-er"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.core_network.name
+
+  type                       = "ExpressRoute"
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.nv-hub-er-gw.id
+  authorization_key          = azurerm_express_route_circuit_authorization.er-to-er-gw.authorization_key
+  express_route_circuit_id   = azurerm_express_route_circuit.main.id
+}
