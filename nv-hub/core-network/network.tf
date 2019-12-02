2f6a0f43dcb7797ea1715373a7549bd459cba047
sidekicke <55448174+sidekicke@users.noreply.github.com>
2019-12-02 15:22:46 +0100

    added siemens vnet peering (#21)

--- nv-hub/core-network/network.tf
+++ nv-hub/core-network/network.tf
@@ -53,10 +53,10 @@ resource "azurerm_virtual_network_peering" "nv-hub_to_infra" {
   allow_gateway_transit        = true
 }
 
-resource "azurerm_virtual_network_peering" "nv-hub_vpn_to_siemens" {
-  name                         = "nv-hub_vpn_to_infra"
+resource "azurerm_virtual_network_peering" "nv-hub_to_siemens" {
+  name                         = "nv-hub_to_siemens"
   resource_group_name          = azurerm_resource_group.core_network.name
-  virtual_network_name         = azurerm_virtual_network.core_vnet_vpn.name
+  virtual_network_name         = azurerm_virtual_network.core_vnet.name
   remote_virtual_network_id    = var.remote_siemens_vnet
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
--- nv-hub/core-network/vpn_gateway.tf
+++ nv-hub/core-network/vpn_gateway.tf
@@ -1,45 +1,3 @@
-# core_vnet_vpn
-resource "azurerm_virtual_network" "core_vnet_vpn" {
-  resource_group_name = azurerm_resource_group.core_network.name
-  location            = var.location
-  name                = "core_vnet_vpn"
-  address_space       = ["10.42.1.0/24"]
-  dns_servers         = ["10.40.250.4", "10.40.250.5"]
-  tags                = merge(var.default_tags, {})
-}
-
-
-resource "azurerm_subnet" "GatewaySubnetVPN" {
-  name                 = "GatewaySubnet"
-  resource_group_name  = azurerm_resource_group.core_network.name
-  virtual_network_name = azurerm_virtual_network.core_vnet_vpn.name
-  address_prefix       = "10.42.1.0/24"
-}
-
-resource "azurerm_public_ip" "public-vpn-gw" {
-  name                = "public-vpn-gw"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.core_network.name
-
-  allocation_method = "Dynamic"
-}
-
-resource "azurerm_virtual_network_gateway" "nv-hub-vpn-gw" {
-  name                = "nw-hub-vpn-gw"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.core_network.name
-
-  type = "Vpn"
-
-  sku = "VpnGw3"
-
-  ip_configuration {
-    name                 = "vpn-gw-config"
-    public_ip_address_id = azurerm_public_ip.public-vpn-gw.id
-    subnet_id            = azurerm_subnet.GatewaySubnetVPN.id
-  }
-}
-
 # core_vnet VPN
 resource "azurerm_public_ip" "public_vpn_gw_core" {
   name                = "public-vpn-gw-core"
@@ -56,7 +14,7 @@ resource "azurerm_virtual_network_gateway" "nv_hub_vpn_gw_core" {
 
   type = "Vpn"
 
-  sku = "VpnGw1"
+  sku = "VpnGw3"
 
   ip_configuration {
     name                 = "vpn-gw-config-core"
