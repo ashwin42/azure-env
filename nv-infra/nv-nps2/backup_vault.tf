5f3fbab3af2dd448830d8de65e265f40c06df4ce
Mihajlo Manojlov <mihajlo.manojlov@northvolt.com>
2020-06-22 13:38:28 +0200

    NPS2 setup

---
+++ nv-infra/nv-nps2/backup_vault.tf
@@ -0,0 +1,46 @@
+resource "azurerm_recovery_services_vault" "nv-nps2" {
+  name                = "nv-nps-recovery-vault2"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
+}
+
+resource "azurerm_recovery_services_protection_policy_vm" "daily" {
+  name                = "daily"
+  resource_group_name = var.resource_group_name
+  recovery_vault_name = azurerm_recovery_services_vault.nv-nps2.name
+
+  backup {
+    frequency = "Daily"
+    time      = "23:00"
+  }
+
+  retention_daily {
+    count = 30
+  }
+
+  retention_weekly {
+    count    = 12
+    weekdays = ["Sunday"]
+  }
+
+  retention_monthly {
+    count    = 12
+    weekdays = ["Sunday"]
+    weeks    = ["Last"]
+  }
+
+  retention_yearly {
+    count    = 1
+    weekdays = ["Sunday"]
+    weeks    = ["Last"]
+    months   = ["December"]
+  }
+}
+
+output "recovery_services" {
+  value = {
+    recovery_vault_name        = "${azurerm_recovery_services_vault.nv-nps2.name}"
+    protection_policy_daily_id = "${azurerm_recovery_services_protection_policy_vm.daily.id}"
+  }
+}
---
+++ nv-infra/nv-nps2/variables.tf
@@ -0,0 +1,44 @@
+// Vars sourced from inputs (terragrunt.hcl)
+variable "resource_group_name" {}
+
+variable "location" {}
+
+// Locally defined vars
+variable "default_tags" {
+  default = {
+    repo = "azure-env/nv_infra/nv_nps2"
+  }
+}
+
+variable "name" {
+  default = "nps2"
+}
+
+variable "vault_id" {
+  default = ""
+}
+
+variable "recovery_vault_name" {
+  default = "nv-nps-recovery-vault2"
+}
+
+variable "recovery_vault_resource_group" {
+  default = "nv_nps2"
+}
+
+variable "backup_policy_id" {
+  default = ""
+}
+
+variable "vm_size" {
+  default = "Standard_DS1_v2"
+}
+
+variable "managed_disk_type" {
+  default = ""
+}
+
+variable "ipaddress" {
+  default = "10.44.3.39"
+}
+
