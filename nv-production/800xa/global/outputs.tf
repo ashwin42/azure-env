7d4e8bd73fe0dc954676489ac81b09e049b0122d
MihajloManojlovNV <66121823+MihajloManojlovNV@users.noreply.github.com>
2020-09-12 19:29:13 +0200

    Move common stuff to global dir

---
+++ nv-production/800xa/global/outputs.tf
@@ -0,0 +1,24 @@
+output "resource_group" {
+  value = azurerm_resource_group.abb_800xa
+}
+
+output "virtual_network" {
+  value = azurerm_virtual_network.abb_800xa
+}
+
+output "subnet_1" {
+  value = azurerm_subnet.abb_800xa_1
+}
+
+output "subnet_2" {
+  value = azurerm_subnet.abb_800xa_2
+}
+
+output "subnet_3" {
+  value = azurerm_subnet.abb_800xa_3
+}
+
+output "vnet_peering" {
+  value = azurerm_virtual_network_peering.abb800xa_to_nv-hub
+}
+
