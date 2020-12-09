6b4b42a20a3f77e36fd4dd3d7d5e8268fd484093
MihajloManojlovNV <mihajlo.manojlov@northvolt.com>
2020-12-09 16:54:44 +0100

    Fix nv-automation-iot

--- northvolt0/nv-automation-iot/iot.tf
+++ northvolt0/nv-automation-iot/iot.tf
@@ -1,9 +1,9 @@
 resource "azurerm_storage_account" "northvolt_iot" {
-  name                     = "northvoltiot"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                              = "northvoltiot"
+  resource_group_name               = azurerm_resource_group.main.name
+  location                          = azurerm_resource_group.main.location
+  account_tier                      = "Standard"
+  account_replication_type          = "LRS"
   enable_advanced_threat_protection = true
 }
 
