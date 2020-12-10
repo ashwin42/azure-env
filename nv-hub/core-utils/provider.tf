bfe5cde70e4d55c8675fbf045a8f8b3af5c6fa5f
MihajloManojlovNV <mihajlo.manojlov@northvolt.com>
2020-12-10 13:56:41 +0100

    Fix nv-hub

--- nv-hub/core-utils/provider.tf
+++ nv-hub/core-utils/provider.tf
@@ -12,8 +12,10 @@ data "terraform_remote_state" "core-network" {
   backend = "azurerm"
 
   config = {
+    resource_group_name  = "nv-hub-core"
     storage_account_name = "nvhubtfstate"
     container_name       = "nv-tf-state"
     key                  = "core-network/terraform.tfstate"
+    subscription_id      = "4312dfc3-8ec3-49c4-b95e-90a248341dd5"
   }
 }
