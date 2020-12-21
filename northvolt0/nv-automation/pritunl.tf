8d1e888bb3ea3e22c458b8c62391c27dc0c7637f
MihajloManojlovNV <mihajlo.manojlov@northvolt.com>
2020-12-21 18:55:47 +0100

    Add Flexlink TIA server - HELP-9860

--- northvolt0/nv-automation/pritunl.tf
+++ northvolt0/nv-automation/pritunl.tf
@@ -460,6 +460,30 @@ resource "azurerm_network_security_group" "pritunl" {
   }
 
   security_rule {
+    name                       = "Allow_Inbound_Flexlink"
+    priority                   = 158
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "UDP"
+    source_port_range          = "*"
+    destination_port_range     = "17791"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow_Outbound_Flexlink"
+    priority                   = 159
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "10.101.2.212/32"
+  }
+
+  security_rule {
     name                       = "Allow_Outbound_Northvolt_RDP"
     priority                   = 200
     direction                  = "Outbound"
