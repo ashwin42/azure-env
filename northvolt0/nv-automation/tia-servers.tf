7724ab56dc2b0f094ae334b5ea5b959ec65600f2
MihajloManojlovNV <mihajlo.manojlov@northvolt.com>
2020-12-21 18:32:23 +0100

    Add Sejong TIA server - HELP-9861

--- northvolt0/nv-automation/tia-servers.tf
+++ northvolt0/nv-automation/tia-servers.tf
@@ -227,3 +227,25 @@ module "tia_flour" {
   subscription_id     = var.subscription_id
 }
 
+# -- Sejong --
+data "azurerm_key_vault_secret" "tia_sejong" {
+  name         = "tia-sejong-nvadmin"
+  key_vault_id = data.azurerm_key_vault.nv_core.id
+}
+
+module "tia_sejong" {
+  source              = "../modules/tia-server"
+  name                = "sejong"
+  ipaddress           = "10.101.2.211"
+  password            = data.azurerm_key_vault_secret.tia_sejong.value
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = local.nv_automation_1
+  dns_zone            = azurerm_dns_zone.tia_nvlt_co.name
+  vault_id            = data.azurerm_key_vault.nv_core.id
+  recovery_vault_name = data.terraform_remote_state.nv-shared.outputs.recovery_services.recovery_vault_name
+  backup_policy_id    = data.terraform_remote_state.nv-shared.outputs.recovery_services.protection_policy_daily_id
+  ad_join             = true
+  subscription_id     = var.subscription_id
+}
+
