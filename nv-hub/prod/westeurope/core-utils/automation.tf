ff00c444fdc05003ac15a9c7ac16d32097d2c9e5
JohannesHedberg <johanneskhedberg@gmail.com>
2022-07-21 20:37:46 +0200

    TOC-944: removed nv-hub log workspace - vms enrolled to existing one

--- nv-hub/prod/westeurope/core-utils/automation.tf
+++ nv-hub/prod/westeurope/core-utils/automation.tf
@@ -7,16 +7,3 @@ resource "azurerm_automation_account" "nv-hub-automation" {
 
 }
 
-resource "azurerm_log_analytics_workspace" "nv-hub-analytics-log" {
-  name                = "nv-hub-analytics-log"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
-
-resource "azurerm_log_analytics_linked_service" "nv-law-link" {
-  resource_group_name = var.resource_group_name
-  workspace_name      = azurerm_log_analytics_workspace.nv-hub-analytics-log.name
-  resource_id         = azurerm_automation_account.nv-hub-automation.id
-}
