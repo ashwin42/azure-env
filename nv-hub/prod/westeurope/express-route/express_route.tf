11be8bff43f2d35de803bdd869f543c9167403de
MihajloManojlovNV <mihajlo.manojlov@northvolt.com>
2022-07-22 19:15:26 +0200

    TOC-1201 - rewrite expressroute code

--- nv-hub/prod/westeurope/express-route/express_route.tf
+++ nv-hub/prod/westeurope/express-route/express_route.tf
@@ -11,11 +11,12 @@ resource "azurerm_express_route_circuit" "main" {
     family = "MeteredData"
   }
 
-  tags = merge(var.default_tags, {})
+  tags = merge(var.default_tags, var.repo_tag, var.module_tag, var.env_tag, var.tags, {})
 }
 
 output "labs_express_route_skey" {
-  value = azurerm_express_route_circuit.main.service_key
+  value     = azurerm_express_route_circuit.main.service_key
+  sensitive = true
 }
 
 resource "azurerm_express_route_circuit_authorization" "er-to-er-gw" {
@@ -25,12 +26,32 @@ resource "azurerm_express_route_circuit_authorization" "er-to-er-gw" {
 }
 
 resource "azurerm_virtual_network_gateway_connection" "er-gw-to-er" {
-  name                = "er-gw-to-er"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
+  name                       = "er-gw-to-er"
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
   type                       = "ExpressRoute"
   virtual_network_gateway_id = azurerm_virtual_network_gateway.nv-hub-er-gw.id
   authorization_key          = azurerm_express_route_circuit_authorization.er-to-er-gw.authorization_key
   express_route_circuit_id   = azurerm_express_route_circuit.main.id
+  tags                       = merge(var.default_tags, var.repo_tag, var.module_tag, var.env_tag, var.tags, {})
+
+  lifecycle {
+    ignore_changes = [authorization_key]
+  }
+}
+
+resource "azurerm_express_route_circuit_peering" "this" {
+  peering_type                  = "AzurePrivatePeering"
+  express_route_circuit_name    = azurerm_express_route_circuit.main.name
+  resource_group_name           = var.resource_group_name
+  peer_asn                      = 1299
+  primary_peer_address_prefix   = "10.225.225.132/30"
+  secondary_peer_address_prefix = "10.225.225.136/30"
+  vlan_id                       = 102
+
+  microsoft_peering_config {
+    advertised_public_prefixes = []
+    customer_asn               = 0
+    routing_registry_name      = "NONE"
+  }
 }
