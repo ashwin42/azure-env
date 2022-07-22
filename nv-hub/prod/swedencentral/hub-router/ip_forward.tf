66db769ecb6ee5aaf6cc437c3a676cc5c6bb67c6
MihajloManojlovNV <mihajlo.manojlov@northvolt.com>
2022-07-22 19:09:27 +0200

    TOC-1199 - swedencentral hub-router

---
+++ nv-hub/prod/swedencentral/hub-router/ip_forward.tf
@@ -0,0 +1,18 @@
+resource "azurerm_virtual_machine_extension" "enable-routes" {
+  name                 = "enable-iptables-routes"
+  virtual_machine_id   = azurerm_virtual_machine.main.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+
+  settings = <<SETTINGS
+    {
+        "fileUris": [
+        "https://raw.githubusercontent.com/mspnp/reference-architectures/master/scripts/linux/enable-ip-forwarding.sh"
+        ],
+        "commandToExecute": "bash enable-ip-forwarding.sh"
+    }
+SETTINGS
+
+  tags = merge(var.default_tags, var.repo_tag, var.module_tag, var.env_tag, var.tags, {})
+}
