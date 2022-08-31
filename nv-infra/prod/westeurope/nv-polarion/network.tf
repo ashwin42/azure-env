529cd5d76f08c544e4060541eb02d5c7fb1d5c4b
fredriknorgren <fredrik.norgren@northvolt.com>
2022-08-31 08:42:41 +0200

    TOC-1283 - removed bastion and reused subnet for Application Gateway

--- nv-infra/prod/westeurope/nv-polarion/network.tf
+++ nv-infra/prod/westeurope/nv-polarion/network.tf
@@ -23,21 +23,21 @@ resource "azurerm_subnet" "nv_polarion_subnet" {
   address_prefix       = "10.44.4.0/28"
 }
 
-resource "azurerm_subnet" "AzureBastionSubnet" {
-  name                 = "AzureBastionSubnet"
+resource "azurerm_subnet" "AG_subnet" {
+  name                 = "AG_subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.nv_polarion_vnet.name
   address_prefix       = "10.44.4.16/28"
 }
 
-resource "azurerm_bastion_host" "polarion_bastion" {
-  name                = "polarion_bastion"
-  location            = var.location
-  resource_group_name = var.resource_group_name
+#resource "azurerm_bastion_host" "polarion_bastion" {
+#  name                = "polarion_bastion"
+#  location            = var.location
+#  resource_group_name = var.resource_group_name
 
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.AzureBastionSubnet.id
-    public_ip_address_id = azurerm_public_ip.polarion-ip.id
-  }
-}
+#  ip_configuration {
+#    name                 = "configuration"
+#    subnet_id            = azurerm_subnet.AzureBastionSubnet.id
+#    public_ip_address_id = azurerm_public_ip.polarion-ip.id
+#  }
+#}
