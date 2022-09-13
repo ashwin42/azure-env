0043bb3213b0c093c7a15f05eb8f0c3b6ba66d18
JohannesHedberg <johanneskhedberg@gmail.com>
2022-09-13 09:58:20 +0200

    TOC-1001: fw rules for ews01 - FL.P1 ITnetwork

--- nv-infra/prod/westeurope/plc-engineering-workstations/nv-plc-ews01/firewall.ps1
+++ nv-infra/prod/westeurope/plc-engineering-workstations/nv-plc-ews01/firewall.ps1
@@ -1,3 +1,4 @@
 netsh advfirewall firewall add rule name="Octoplant Server 64001-64006" dir=in action=allow protocol=TCP localport=64001-64006
 netsh advfirewall firewall add rule name="Octoplant Server 64021" dir=in action=allow protocol=TCP localport=64021
 
+netsh advfirewall firewall add rule name="FL.P1 IT-Network" dir=in action=allow protocol=TCP remoteip=10.101.200.128/26
