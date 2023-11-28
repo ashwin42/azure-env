151be2bb962264ea3e2df91b40017e866f77d880
Johannes Hedberg <johanneskhedberg@gmail.com>
2023-11-28 09:28:54 +0100

    HELP-119177: removed old lasernet code

--- northvolt0/prod/northeurope/lasernet/network.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_subnet" "gateway_subnet" {
-  name                 = "GatewaySubnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = var.virtual_network_name
-  address_prefix       = var.gateway_subnet_prefix
-}
-
