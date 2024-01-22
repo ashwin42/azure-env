58d7fc4206f09373790642f4a9f9314d4a9fbb16
Johannes Kimmo Hedberg <johannes.hedberg@northvolt.com>
2024-01-22 10:53:23 +0000

    TOC-2086: nv-plc-ews rg & rv updated

--- nv-infra/prod/westeurope/plc-engineering-workstations/nv-plc-ews02/firewall.ps1
+++
@@ -1,4 +0,0 @@
-netsh advfirewall firewall add rule name="Octoplant Server 64001-64006" dir=in action=allow protocol=TCP localport=64001-64006
-netsh advfirewall firewall add rule name="Octoplant Server 64021" dir=in action=allow protocol=TCP localport=64021
-
-netsh advfirewall firewall add rule name="FL.F1 IT-Network" dir=in action=allow protocol=TCP remoteip=10.101.194.128/26
